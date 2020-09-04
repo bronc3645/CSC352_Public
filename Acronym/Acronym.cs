d540082a40e75f7ff2fb8dda33aec9f33416a689
Bronc3645 <kingdomhlore@gmail.com>
2020-09-04 17:39:31 -0600

    progress as of 9/4/20

--- Acronym/Acronym.cs
+++ Acronym/Acronym.cs
@@ -1,12 +1,34 @@
 ﻿namespace Acronym
 {
     using System;
+    using System.Text;
 
     public static class Acronym
     {
         public static string Abbreviate(string phrase)
         {
-            throw new NotImplementedException();
+            StringBuilder sb = new StringBuilder();
+
+            String[] splitphrase = phrase.Split(' ');
+
+            foreach(String subphrase in splitphrase)
+            {
+                if (subphrase.Length != 0)
+                {
+                    char firstchar = subphrase[0];
+
+                    if (firstchar == '_')
+                    {
+                        sb.Append(subphrase[1]);
+                    }
+                    else if (firstchar != '-')
+                    {
+                        sb.Append(firstchar);
+                    }
+                }
+            }
+
+            return sb.ToString().ToUpper();
         }
     }
 }
