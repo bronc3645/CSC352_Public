4255bea51f80da68b3b071ac3e4fac98c22af018
Bronc3645 <kingdomhlore@gmail.com>
2020-09-04 17:39:52 -0600

    progress as of 9/2/20

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello there this is a test");
+            Console.WriteLine("Hello World!");
+            for(int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
