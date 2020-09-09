7cf6ccaa34d3bdc0b89d14a5a4508ae33e4be598
Bronc3645 <kingdomhlore@gmail.com>
2020-09-09 17:27:34 -0600

    progress as of 9/9/20
    5:27 PM

--- FunWithLoops/Program.cs
+++ FunWithLoops/Program.cs
@@ -7,42 +7,87 @@
     {
         internal static IEnumerable<int> NumbersOneThruTwentyFive()
         {
-            throw new NotImplementedException();
+            for (int i = 1; i <= 25; i++)
+            {
+                yield return i;
+            }
         }
 
         internal static IEnumerable<int> EvenNumbersOneThruTwentyFive()
         {
-            throw new NotImplementedException();
+            for (int i = 2; i <= 25; i+=2)
+            {
+                yield return i;
+            }
         }
 
         internal static IEnumerable<int> OddNumbersOneThruTwentyFive()
         {
-            throw new NotImplementedException();
+            for (int i = 1; i <= 25; i += 2)
+            {
+                yield return i;
+            }
         }
 
         internal static IEnumerable<int> PrimeNumbersOneThruTwentyFive()
         {
-            throw new NotImplementedException();
+            for (int i = 1; i <= 25; i ++)
+            {
+                if (IsPrime(i))
+                {
+                    yield return i;
+                }
+            }
         }
 
         internal static bool IsPrime(int number)
         {
-            throw new NotImplementedException();
+            bool test = false;
+            if (number == 2)
+            {
+                test = true;
+            }
+            else if (number > 2)
+            {
+                test = true;
+                for (int i = 2; i <= number/2; i++)
+                {
+                    if (number % i == 0)
+                    {
+                        test = false;
+                        break;
+                    }
+                }
+            }
+            return test;
+
         }
 
         internal static IEnumerable<int> GetNumbersOneThru(int thruInclusive)
         {
-            throw new NotImplementedException();
+            for (int i = 1; i <= thruInclusive; i ++)
+            {
+                yield return i;
+            }
         }
 
         internal static IEnumerable<int> GetRangeThruInclusive(int start, int end)
         {
-            throw new NotImplementedException();
+            for (int i = start; i <= end; i ++)
+            {
+                yield return i;
+            }
         }
 
         internal static IEnumerable<int> GetPrimesInRangeInclusive(int start, int end)
         {
-            throw new NotImplementedException();
+            for (int i = start; i <= end; i++)
+            {
+                if (IsPrime(i))
+                {
+                    yield return i;
+                }
+            }
         }
     }
 }
