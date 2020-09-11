45403e30cf6ecc3075c72b47ddeda2b3ef189593
Bronc3645 <kingdomhlore@gmail.com>
2020-09-11 17:28:59 -0600

    progress as of 9/11/20
    5:28 pm
    added createPhoneNumber project

---
+++ CreatePhoneNumber/unitTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace CreatePhoneNumber
+{
+    [TestFixture]
+    public class unitTest
+    {
+        [Test]
+        public void someTest()
+        {
+            Assert.IsTrue(true);
+        }
+
+        [Test]
+        public void someOtherTest()
+        {
+            Assert.IsTrue(false);
+        }
+    }
+}
