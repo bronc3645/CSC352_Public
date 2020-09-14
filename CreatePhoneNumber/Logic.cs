87eec79ddaaa15a5905dd16f2182c4f3b02570b8
Bronc3645 <kingdomhlore@gmail.com>
2020-09-14 17:33:41 -0600

    progress as of 9/14/20
    5:33 PM

--- CreatePhoneNumber/Logic.cs
+++ CreatePhoneNumber/Logic.cs
@@ -6,5 +6,32 @@ namespace CreatePhoneNumber
 {
     class Logic
     {
+        internal static string TranslateToPhoneNumbers(int[] numbers)
+        {
+            //String result = "(" + numbers[0]+numbers[1]+numbers[2]+") "+numbers[3]+numbers[4]+numbers[5]+"-"+numbers[6]+numbers[7]+numbers[8]+numbers[9];
+            //return result;
+            Queue<string> stack = new Queue<string>();
+            stack.Enqueue("(");
+            for(int i = 0; i < 3; i++)
+            {
+                stack.Enqueue(""+numbers[i]);
+            }
+            stack.Enqueue(") ");
+            for(int i = 3; i < 6; i++)
+            {
+                stack.Enqueue(""+numbers[i]);
+            }
+            stack.Enqueue("-");
+            for(int i = 6; i < 10; i++)
+            {
+                stack.Enqueue("" + numbers[i]);
+            }
+            StringBuilder sb = new StringBuilder();
+            while (stack.Count != 0)
+            {
+                sb.Append(stack.Dequeue());
+            }
+            return sb.ToString();
+        }
     }
 }
--- CreatePhoneNumber/unitTest.cs
+++ CreatePhoneNumber/unitTest.cs
@@ -6,16 +6,12 @@ namespace CreatePhoneNumber
     [TestFixture]
     public class unitTest
     {
-        [Test]
-        public void someTest()
+        [TestCase(new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 0 }, "(123) 456-7890")]
+        [TestCase(new int[] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, "(000) 000-0000")]
+        public void ValidFormat(int[] numbers, String expected)
         {
-            Assert.IsTrue(true);
-        }
-
-        [Test]
-        public void someOtherTest()
-        {
-            Assert.IsTrue(false);
+            String actual = Logic.TranslateToPhoneNumbers(numbers);
+            Assert.That(actual, Is.EqualTo(expected));
         }
     }
 }
