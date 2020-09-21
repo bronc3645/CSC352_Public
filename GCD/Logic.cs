2b1b107195e380344a763e8dd17f1c8863d19aed
Bronc3645 <kingdomhlore@gmail.com>
2020-09-21 17:25:59 -0600

    progress as of 9/21/20
    5:25

--- GCD/Logic.cs
+++ GCD/Logic.cs
@@ -6,14 +6,45 @@ namespace GCD
     {
         public static long gcd(long x, long y)
         {
-            if (y == 0)
+            /*if (y == 0)
             {
                 return x;
             }
             else
             {
                 return gcd(y, x % y);
+            }*/
+
+            /*if (x < y)
+            {
+                long temp = y;
+                y = x;
+                x = temp;
             }
+
+            long result = 0;
+
+            for(long i = y; i > 0; i--)
+            {
+                if (x % i == 0 && y % i == 0)
+                {
+                    result = i;
+                    break;
+                }
+            }
+
+            return result;*/
+
+            long gcd=0;
+
+            while (y != 0)
+            {
+                gcd = y;
+                y = x % y;
+                x = gcd;
+            }
+
+            return gcd;
         }
     }
 }
