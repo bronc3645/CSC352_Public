df05b0a7467d98062eaae8867b1f8b245470350b
Bronc3645 <kingdomhlore@gmail.com>
2020-09-23 17:29:05 -0600

    progress as of 9/23/20
    5:29

---
+++ FunWithClasses/person.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Text;
+
+namespace FunWithClasses
+{
+    public class person
+    {
+        private string nFirstname;
+        public String firstname
+        {
+            get
+            {
+                return this.nFirstname;
+            }
+
+            set
+            {
+                StringBuilder sb = new StringBuilder();
+
+                sb.Append(value);
+                sb[0] = char.ToUpper(value[0]);
+
+                /*for(int i = 0; i < value.Length; i++)
+                {
+                    if (i == 0)
+                    {
+                        sb.Append(char.ToUpper(value[i]));
+                    }
+                    else
+                    {
+                        sb.Append(value[i]);
+                    }
+                }*/
+
+                this.nFirstname = sb.ToString();
+            }
+        }
+        string Lastname
+        {
+            set;
+            get;
+        }
+        DateTime birthdate
+        {
+            get;
+            set;
+        }
+        public override int GetHashCode()
+        {
+            return this.firstname.GetHashCode() + this.Lastname.GetHashCode();
+        }
+
+        public override bool Equals(object obj)
+        {
+            bool isEqual = false;
+            person other = obj as person;
+            if (other == null)
+            {
+                isEqual = false;
+            }
+            else
+            {
+                if (this.GetHashCode() == other.GetHashCode())
+                {
+                    if (this.birthdate == other.birthdate)
+                    {
+                        isEqual = true;
+                    }
+                    else
+                    {
+                        isEqual = false;
+                    }
+                }
+                else
+                {
+                    isEqual = false;
+                }
+            }
+            return isEqual;
+        }
+    }
+}
---
+++ FunWithClasses/unitTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+
+namespace FunWithClasses
+{
+    [TestFixture]
+    class unitTest
+    {
+        [TestCase("ace","Ace")]
+        [TestCase("jerry","Jerry")]
+        public void testFirstNameProperty(string input, string expected)
+        {
+            person person = new person();
+
+            person.firstname = input;
+
+            Assert.That(person.firstname, Is.EqualTo(expected));
+        }
+    }
+}
