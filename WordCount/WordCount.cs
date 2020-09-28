c5e1a530968c891348cc45af92f8418d6f5b2e31
Bronc3645 <kingdomhlore@gmail.com>
2020-09-28 17:35:35 -0600

    progress as of 9/28/20
    5:35 PM

--- WordCount/WordCount.cs
+++ WordCount/WordCount.cs
@@ -11,7 +11,65 @@ namespace WordCount
     {
         public static IDictionary<string, int> CountWords(string phrase)
         {
-            throw new NotImplementedException();
+            IDictionary<string, int> result = new Dictionary<string, int>();
+
+            phrase = phrase.ToLower();
+
+            string[] split = phrase.Split(new string[] { " ", "," , "\n","_"}, StringSplitOptions.RemoveEmptyEntries);
+
+            for (int i = 0; i < split.Length; i++)
+            {
+                split[i] = sanitizePhrase(split[i]);
+                if (split[i] != "")
+                {
+                    if (result.ContainsKey(split[i]))
+                    {
+                        result[split[i]] = result[split[i]] + 1;
+                    }
+                    else
+                    {
+                        result[split[i]] = 1;
+                    }
+                }
+            }
+            return result;
+        }
+
+        private static string sanitizePhrase(string input)
+        {
+            string[] forbiddenCharacters =
+            {
+                ":",
+                "!",
+                "@",
+                "#",
+                "$",
+                "%",
+                "^",
+                "&",
+                "*",
+                "(",
+                ")",
+                "?",
+                "-",
+                "=",
+                "+",
+                "/",
+                "\\",
+                "."
+            };
+            foreach (string forbiddenchar in forbiddenCharacters)
+            {
+                input = input.Replace(forbiddenchar, string.Empty);
+            }
+            if (input.IndexOf("'") != input.LastIndexOf("'"))
+            {
+                while (input.Contains("'"))
+                {
+                    input=input.Replace("'", string.Empty);
+                }
+            }
+            return input;
         }
     }
 }
