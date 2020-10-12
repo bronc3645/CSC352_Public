b0cbceaa3c04dda9315684bf42eaf27846fd5fef
Bronc3645 <kingdomhlore@gmail.com>
2020-10-12 17:40:30 -0600

    progress as of 10/12/20
    5:40 PM
    evaluator works

---
+++ calculator.logic.unitTest/EvaluatorTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace calculator.logic.unitTest
+{
+    [TestFixture]
+    public class EvaluatorTest
+    {
+        [TestCase("1 2 + 3 +", 6)]
+        [TestCase("1 2 + 3 *", 9)]
+        [TestCase("3 4 2 * 1 5 - 2 3 ^ ^ / +", 3.0001220703125)]
+        public void testEvaluation(string input, double expected)
+        {
+            double actual = Evaluator.Eval(input);
+
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+    }
+}
--- calculator.logic/Evaluator.cs
+++ calculator.logic/Evaluator.cs
@@ -6,7 +6,77 @@ using System.Threading.Tasks;
 
 namespace calculator.logic
 {
-    class Evaluator
+    public class Evaluator
     {
+        public static double Eval(string rpn)
+        {
+            string[] split = rpn.Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries);
+            Stack<string> stack = new Stack<string>();
+            foreach(string token in split)
+            {
+                if (isOperator(token)){
+                    double right = double.Parse(stack.Pop());
+                    double left = double.Parse(stack.Pop());
+                    switch (token)
+                    {
+                        case "+":
+                            {
+                                stack.Push(""+(left + right));
+                                break;
+                            }
+                        case "-":
+                            {
+                                stack.Push("" + (left - right));
+                                break;
+                            }
+                        case "*":
+                            {
+                                stack.Push("" + (left * right));
+                                break;
+                            }
+                        case "/":
+                            {
+                                stack.Push("" + (left / right));
+                                break;
+                            }
+                        case "^":
+                            {
+                                stack.Push("" + (Math.Pow(left, right)));
+                                break;
+                            }
+                        default:
+                            {
+                                break;
+                            }
+                    }
+                }
+                else{
+                    stack.Push(token);
+                }
+            }
+            if (stack.Count == 1)
+            {
+                return double.Parse(stack.Pop());
+            }
+            else
+            {
+                throw new Exception();
+            }
+        }
+
+        private static bool isOperator(string token)
+        {
+            switch (token)
+            {
+                case "+":
+                case "-":
+                case "*":
+                case "/":
+                case "^":
+                    return true;
+                default:
+                    return false;
+            }
+        }
     }
 }
