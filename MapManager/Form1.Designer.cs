d506daf405273cf57c921b7bac7a9f33c88d2f37
Bronc3645 <kingdomhlore@gmail.com>
2020-10-28 17:39:12 -0600

    progress as of 10/28/20
    need to center the asset after clicking it

--- MapManager/Form1.Designer.cs
+++ MapManager/Form1.Designer.cs
@@ -29,24 +29,17 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.assetBox = new System.Windows.Forms.PictureBox();
-            this.mapPictureBox = new System.Windows.Forms.PictureBox();
             this.primaryVerticalLayout = new System.Windows.Forms.TableLayoutPanel();
             this.PrimaryHorizontalTable = new System.Windows.Forms.TableLayoutPanel();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.mapPictureBox = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.assetBox)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.mapPictureBox)).BeginInit();
             this.primaryVerticalLayout.SuspendLayout();
             this.PrimaryHorizontalTable.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.mapPictureBox)).BeginInit();
             this.SuspendLayout();
-            // 
-            // comboBox1
-            // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(3, 313);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(193, 24);
-            this.comboBox1.TabIndex = 1;
             // 
             // assetBox
             // 
@@ -54,21 +47,11 @@
             this.assetBox.Image = ((System.Drawing.Image)(resources.GetObject("assetBox.Image")));
             this.assetBox.Location = new System.Drawing.Point(3, 3);
             this.assetBox.Name = "assetBox";
-            this.assetBox.Size = new System.Drawing.Size(193, 304);
+            this.assetBox.Size = new System.Drawing.Size(196, 303);
             this.assetBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.assetBox.TabIndex = 2;
             this.assetBox.TabStop = false;
-            // 
-            // mapPictureBox
-            // 
-            this.mapPictureBox.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.mapPictureBox.Image = ((System.Drawing.Image)(resources.GetObject("mapPictureBox.Image")));
-            this.mapPictureBox.Location = new System.Drawing.Point(208, 3);
-            this.mapPictureBox.Name = "mapPictureBox";
-            this.mapPictureBox.Size = new System.Drawing.Size(853, 620);
-            this.mapPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.mapPictureBox.TabIndex = 0;
-            this.mapPictureBox.TabStop = false;
+            this.assetBox.Click += new System.EventHandler(this.assetBox_Click);
             // 
             // primaryVerticalLayout
             // 
@@ -76,7 +59,7 @@
             this.primaryVerticalLayout.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
             this.primaryVerticalLayout.ColumnCount = 2;
             this.primaryVerticalLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
-            this.primaryVerticalLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.primaryVerticalLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
             this.primaryVerticalLayout.Controls.Add(this.PrimaryHorizontalTable, 0, 0);
             this.primaryVerticalLayout.Controls.Add(this.mapPictureBox, 1, 0);
             this.primaryVerticalLayout.Dock = System.Windows.Forms.DockStyle.Fill;
@@ -84,7 +67,7 @@
             this.primaryVerticalLayout.Name = "primaryVerticalLayout";
             this.primaryVerticalLayout.RowCount = 1;
             this.primaryVerticalLayout.RowStyles.Add(new System.Windows.Forms.RowStyle());
-            this.primaryVerticalLayout.Size = new System.Drawing.Size(919, 626);
+            this.primaryVerticalLayout.Size = new System.Drawing.Size(1127, 826);
             this.primaryVerticalLayout.TabIndex = 3;
             // 
             // PrimaryHorizontalTable
@@ -95,41 +78,80 @@
             this.PrimaryHorizontalTable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
             this.PrimaryHorizontalTable.Controls.Add(this.assetBox, 0, 0);
             this.PrimaryHorizontalTable.Controls.Add(this.comboBox1, 0, 1);
+            this.PrimaryHorizontalTable.Controls.Add(this.comboBox2, 0, 2);
             this.PrimaryHorizontalTable.Dock = System.Windows.Forms.DockStyle.Fill;
             this.PrimaryHorizontalTable.Location = new System.Drawing.Point(3, 3);
             this.PrimaryHorizontalTable.Name = "PrimaryHorizontalTable";
-            this.PrimaryHorizontalTable.RowCount = 2;
-            this.PrimaryHorizontalTable.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.PrimaryHorizontalTable.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.PrimaryHorizontalTable.Size = new System.Drawing.Size(199, 620);
+            this.PrimaryHorizontalTable.RowCount = 3;
+            this.PrimaryHorizontalTable.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.PrimaryHorizontalTable.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.PrimaryHorizontalTable.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.PrimaryHorizontalTable.Size = new System.Drawing.Size(202, 820);
             this.PrimaryHorizontalTable.TabIndex = 4;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.CausesValidation = false;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Agents",
+            "Weapons",
+            "Abilities"});
+            this.comboBox1.Location = new System.Drawing.Point(3, 312);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(193, 24);
+            this.comboBox1.TabIndex = 1;
+            this.comboBox1.TabStop = false;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(3, 342);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(196, 24);
+            this.comboBox2.TabIndex = 3;
+            this.comboBox2.TabStop = false;
+            // 
+            // mapPictureBox
+            // 
+            this.mapPictureBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.mapPictureBox.Image = ((System.Drawing.Image)(resources.GetObject("mapPictureBox.Image")));
+            this.mapPictureBox.Location = new System.Drawing.Point(211, 3);
+            this.mapPictureBox.Name = "mapPictureBox";
+            this.mapPictureBox.Size = new System.Drawing.Size(913, 820);
+            this.mapPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.mapPictureBox.TabIndex = 0;
+            this.mapPictureBox.TabStop = false;
+            this.mapPictureBox.MouseMove += new System.Windows.Forms.MouseEventHandler(this.mapPictureBox_MouseMove);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
-            this.ClientSize = new System.Drawing.Size(919, 626);
+            this.ClientSize = new System.Drawing.Size(1127, 826);
             this.Controls.Add(this.primaryVerticalLayout);
             this.Name = "Form1";
             this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Show;
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.assetBox)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.mapPictureBox)).EndInit();
             this.primaryVerticalLayout.ResumeLayout(false);
             this.primaryVerticalLayout.PerformLayout();
             this.PrimaryHorizontalTable.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.mapPictureBox)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-        private System.Windows.Forms.ComboBox comboBox1;
         private System.Windows.Forms.PictureBox assetBox;
-        private System.Windows.Forms.PictureBox mapPictureBox;
         private System.Windows.Forms.TableLayoutPanel primaryVerticalLayout;
         private System.Windows.Forms.TableLayoutPanel PrimaryHorizontalTable;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.PictureBox mapPictureBox;
     }
 }
 
--- MapManager/Form1.cs
+++ MapManager/Form1.cs
@@ -13,11 +13,77 @@ namespace MapManager
 {
     public partial class Form1 : Form
     {
+        Bitmap renderImange = null;
+        Bitmap overlayImage = null;
+        Bitmap combinedImage = null;
+        Point overlayLocation = new Point(0,0);
+
         public Form1()
         {
             InitializeComponent();
-            assetBox.AllowDrop = true;
-            mapPictureBox.AllowDrop = true;
+            renderImange = new Bitmap(mapPictureBox.Image);
+        }
+
+        private void assetBox_Click(object sender, EventArgs e)
+        {
+            overlayImage = new Bitmap(assetBox.Image);
+            mapPictureBox.Cursor = Cursors.Cross;
+        }
+
+        private void showCombinedImage()
+        {
+            if (renderImange == null && overlayImage == null)
+            {
+                return;
+            }
+
+            mapPictureBox.Image = renderImange;
+
+            if (combinedImage != null)
+            {
+                combinedImage.Dispose();
+                combinedImage = null;
+            }
+
+            combinedImage = new Bitmap(renderImange);
+
+            using (Graphics combiner = Graphics.FromImage(combinedImage))
+            {
+                combiner.DrawImage(overlayImage, overlayLocation);
+            }
+
+            mapPictureBox.Image = combinedImage;
+
+            /*// If there's no background image, do nothing.
+            if (renderImange == null) return;
+
+            // Copy the background.
+            combinedImage= new Bitmap(renderImange);
+
+            // Add the overlay.
+            if (overlayImage != null)
+            {
+                using (Graphics gr = Graphics.FromImage(combinedImage))
+                {
+                    gr.DrawImage(overlayImage, overlayLocation);
+                }
+            }
+
+            // Display the result.
+            mapPictureBox.Image = combinedImage;*/
+
+        }
+
+        private void mapPictureBox_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (overlayImage == null)
+            {
+                return;
+            }
+            Console.WriteLine(e.X + "  " + e.Y);
+            overlayLocation=new Point(e.X, e.Y);
+
+            showCombinedImage();
         }
     }
 }
